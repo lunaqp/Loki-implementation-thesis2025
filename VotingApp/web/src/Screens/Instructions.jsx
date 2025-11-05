7ab67e8e3f31079cc077cfe744c896050948573d
Aja <ajha@itu.dk>
2025-11-05 16:01:20 +0100

    Added Step by step instructions. Work in progress

--- VotingApp/web/src/Screens/Instructions.jsx
+++ VotingApp/web/src/Screens/Instructions.jsx
@@ -10,24 +10,131 @@ const InstructionsPage = () => {
         <Container>
           <PageTitle>Instructions</PageTitle>
           <PageIntro>
-            Here you can find a step by step guide of how to use the voting
-            system, aswell as Frequently Asked Questions.
+            On this page you will find information about system use, background,
+            coercion resistance and Frequently Asked Questions.
           </PageIntro>
 
           {/* Instructions */}
           <Card accent="blue">
-            <SectionHeader>How to cast a vote</SectionHeader>
+            <SectionHeader>The voting process - Step by step</SectionHeader>
             <StepList>
               <li>
-                1. Open your election and press <b>Vote</b>.
+                <b>Step 1: Welcome </b>
+                <br />
+                This is the first screen you will see when clicking "Vote" in an
+                election. This screen welcomes you to the voting process.
               </li>
-              <li>2. Make your selection and confirm.</li>
-              <li>3. Verify your vote in the app if needed.</li>
-              <li>4. Verify your vote in the app if needed.</li>
+
               <li>
-                5. Verify your vote in the app if needed . Verify your vote in
-                the app if needed Verify your vote in the app if needed Verify
-                your vote in the app if needed
+                <b>Step 2: Have you voted before?</b>
+                <br />
+                In this step, you will be asked to state if you have already
+                voted in this election.
+                <br />
+                - If this is your first time voting in this election, select
+                “No” and you will proceed to select a candidate to vote for.
+                <br />
+                - If you have already cast one or more previous votes and want
+                to change who you voted for, select “Yes”. You will then be
+                directed to the previous votes selection screen.
+                <br />
+                <br />
+                <RedText>
+                  This step is the first part of the coercion resistant
+                  mechanism. If you are being coerced, and have already voted in
+                  this election, you can select “No” and the system will
+                  automatically know not to count the vote you are about to
+                  cast.
+                </RedText>
+              </li>
+
+              <li>
+                <b>Step 3 - YES: Select previous votes </b>
+                <br />
+                This step will only be shown if you answered “Yes” to having
+                voted earlier in this election. This step in combination with
+                step 4 are the main function of the coercion resistant
+                mechanism.
+                <br />
+                <br />
+                The purpose of this step is to confirm that YOU are the one
+                changing your vote and that you are not coerced into doing so.
+                <br />
+                You will be asked to identify all previously cast votes by
+                providing all the image/word-pairs that were displayed after you
+                cast your previous vote(s).
+                <br />
+                - Search through the images of the election period by using the
+                timeline or by scrolling through the hours.
+                <br />
+                - Click the images to select. The selected images will be
+                displayed in a list of “selected images” in the bottom. It is
+                not necessary that the previous images/words are provided in the
+                correct order.
+                <br />
+                - To unselect images, click them in the “selected images” list
+                and they will be deleted from the list.
+                <br />
+                <br />
+                <RedText>
+                  It is crucial that you choose the correct image/word pair, as
+                  if it is wrong the system will still allow you to cast the
+                  vote but will mark the vote as invalid.
+                </RedText>
+              </li>
+
+              <li>
+                <b>Step 3 – NO: Select a candidate to vote for</b>
+                <br />
+                In this step you will choose the candidate to vote for in the
+                given election.
+                <br />
+                Make sure you are voting for the intended candidate before you
+                move on.
+              </li>
+
+              <li>
+                <b>
+                  Step 4: Get the memorable information connected to your vote
+                </b>
+                <br />
+                This step is very important.
+                <br />
+                You will be shown a new image/word, which you will have to
+                remember in case you want to change your vote later. It is
+                crucial that you remember this exact image as you will have to
+                identify it among a lot of other images if you want to change
+                your vote.
+                <br />
+                <br />
+                <RedText>
+                  Make sure you have the information memorised before you move
+                  on! Once you click “Next” you cannot go back.
+                </RedText>
+              </li>
+
+              <li>
+                <b>Step 5: Confirmation</b>
+                <br />
+                This is the last step in the voting process.
+                <br />
+                This is the only confirmation you will see. For security
+                reasons, the system will not send you a receipt e-mail to
+                confirm your choice of candidate.
+                <br />
+                <br />
+                The system will automatically verify that your vote is included
+                in the election. In case there is an error or the vote is not
+                included, the system will send you an email, telling you to
+                revote.
+                <br />
+                <br />
+                <RedText>
+                  After you have completed the voting process, there will be a
+                  timeout of 6 minutes to ensure the systems security mechanisms
+                  work as intended. Be aware that a coercer will know that you
+                  have voted before if they see that you are on timeout.
+                </RedText>
               </li>
             </StepList>
           </Card>
@@ -35,14 +142,35 @@ const InstructionsPage = () => {
           {/* FAQ */}
           <Card accent="slate">
             <SectionHeader>FAQ</SectionHeader>
-            <FAQItem q="Can I verify my vote?">
-              You can verify inclusion via the <b>Verify</b> button on MyPage.
+            <FAQItem q="What does coercion mean?">
+              blablablaaaaa............
             </FAQItem>
-            <FAQItem q="Is my vote private?">
-              Yes. Your ballot is encrypted and anonymized before submission.
+            <FAQItem q="Why do we need coercion resistance in electronic voting systems? ?">
+              blablablaaaaa.............
             </FAQItem>
-            <FAQItem q="What if I lose connection while voting?">
-              Reopen the app and retry. If you’re unsure, use <b>Verify</b>.
+            <FAQItem q="How does LOKI Coercion resistance work?">
+              blablablaaaaa............
+            </FAQItem>
+            <FAQItem q="Why is there a timeout after I have voted?">
+              blablablaaaaa............
+            </FAQItem>
+            <FAQItem q="How can I verify my vote is correctly cast?">
+              blablablaaaaa............
+            </FAQItem>
+            <FAQItem q="What do I do if I receive an email saying my vote is not included?">
+              blablablaaaaa............
+            </FAQItem>
+            <FAQItem q="What if I forgot my memorable information/image?">
+              blablablaaaaa............
+            </FAQItem>
+            <FAQItem q="Should I only remember the image for the most recent vote I cast or all of the images if i revoted multiple times?">
+              blablablaaaaa............
+            </FAQItem>
+            <FAQItem q="What if I am coerced after I have already cast a vote with my true candidate choice?">
+              blablablaaaaa............
+            </FAQItem>
+            <FAQItem q="What if I am coerced before I have managed to cast a vote my true candidate choice?">
+              blablablaaaaa............
             </FAQItem>
           </Card>
           <ButtonBar>
@@ -70,6 +198,11 @@ function FAQItem({ q, children }) {
 }
 
 export default InstructionsPage;
+
+const RedText = styled.p`
+  color: red;
+  margin: 0;
+`;
 
 const Page = styled.div`
   display: flex;
@@ -157,7 +290,7 @@ const StepList = styled.ol`
   padding-left: 0;
   list-style: none;
   li + li {
-    margin-top: 10px;
+    margin-top: 15px;
   }
 
   li {
