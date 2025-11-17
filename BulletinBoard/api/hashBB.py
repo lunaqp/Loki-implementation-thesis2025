51c9de4821eaab3c60f801027e13c781c804bfeb
Aja <ajha@itu.dk>
2025-11-17 09:12:30 +0100

    Added comments in BB

--- BulletinBoard/api/hashBB.py
+++ BulletinBoard/api/hashBB.py
@@ -4,6 +4,15 @@ from modelsBB import Ballot
 Included_fields = {"voterid", "upk", "ctv", "ctlv", "ctlid", "proof"}
 
 def hash_ballot(ballot: Ballot) -> str:
+    """Compute the hash of a ballot.
+    The ballot is converted to a dictionary containing only the fields listed
+    in `Included_fields`.This is to be able to reuse the pydantic "Ballot" with only some of the fields.
+    The dictionary is then serialized to a JSON string and hashed using SHA-256.
+    Args:
+        ballot (Ballot): A Ballot model instance to hash.
+    Returns:
+        str: The SHA-256 hash of the ballot.
+    """
     ballot_with_included_fields = ballot.model_dump(include=Included_fields) #python dict with included fields
     payload = json.dumps(ballot_with_included_fields)
     return hashlib.sha256(payload.encode("utf-8")).hexdigest()
--- BulletinBoard/api/notifications.py
+++ BulletinBoard/api/notifications.py
@@ -1,9 +1,25 @@
+"""This file is used for notifying RA, TS, and VS.
+
+It provides asynchronous helper functions used to notify external
+services: Tallying Server (TS), Voting Server (VS), and
+Registration Authority (RA), about important events in the setup phase.
+"""
 import httpx
 from fastapi import HTTPException
 from coloursBB import BLUE, RED
 
 # Notify TallyingServer and VotingServer of g and order being saved to database.
 async def notify_ts_vs_params_saved():
+    """Notify the Tallying Server and Voting Server that Elgamal parameters are stored.
+    This endpoint is used after the Registration Authority posts the ElGamal
+    parameters on the BB/DB. Both TS and VS can then begin to generate their keys once the
+    parameters become available.
+    Returns:
+        tuple: JSON responses from the Tallying Server and Voting Server.
+    Raises:
+        httpx.HTTPError: If an HTTP transport error occurs while contacting
+        either.
+    """
     print(f"{BLUE}Notifying Tallying and Voting server of Elgamal parameters saved to database") # NOTE: Add try/catch block.
     async with httpx.AsyncClient() as client:
         # Call TallyingServer:
@@ -14,6 +30,16 @@ async def notify_ts_vs_params_saved():
     return resp_TS.json(), resp_VS.json()
 
 async def notify_ra_public_key_saved(service):
+    """Notify the Registration Authority that a public key for a service is stored.
+    This function informs the RA that a new public key (from VS or TS)
+    has been saved in the bulletin board database.
+    Args:
+        service (str): The name of the service whose key was stored.
+    Returns:
+        None
+    Raises:
+        HTTPException: If the RA cannot be contacted/the request fails.
+    """
     async with httpx.AsyncClient() as client:
         try:
             resp = await client.post("http://ra_api:8000/key_ready", json={
